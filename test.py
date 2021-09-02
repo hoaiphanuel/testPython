378ba971ad4bb228231c2afa497a63aee439b975
hoaiphan2626 <hoaiphan@gmail.com>
2021-09-03 04:00:44 +0700

    them doan sắp xếp

--- test.py
+++ test.py
@@ -3,3 +3,11 @@
 #print("*"*100)
 # một hình chữ nhật 10x10
 print((("*"*10)+"\n")*(2*5))
+a=[1,2,3,4,5,6,7,8,3,8,4,4,4,4]
+for i in range(len(a)):
+        for j in range(len(a)):
+                if (a[i]<a[j]):
+                        tam=a[i]
+                        a[i]=a[j]
+                        a[j]=tam
+print(a+1)
---
+++ testTensorFlow.py
@@ -0,0 +1,25 @@
+import epochs as epochs
+import tensorflow as tf
+import numpy as np
+from tensorflow import keras
+import time
+import matplotlib.pyplot as plt
+import imageio
+import random
+
+#du lieu ham y=x*2
+
+x = np.arange(0, 1, 0.01)
+y = x*2
+print(x)
+print(y)
+#gay nhiểu y
+#y = y+ np.random.uniform(-0.3,0.3,x.shape)
+print(y)
+xs = np.array(x,dtype=float)
+ys = np.array(y,dtype=float)
+model = keras.Sequential([keras.layers.Dense(units=1,input_shape=[1])])
+model.compile(optimizer="sgd",loss="mean_squared_error")
+
+model.fit(xs,ys,epochs=500)
+print(model.predict([10]))
